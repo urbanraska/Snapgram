eb907a011fbb14703efc1fd8d5330bd15709d1b8
Subarna Kar <subarnakar1122@gmail.com>
2024-07-13 14:02:01 +0530

    private route added

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,18 +1,26 @@
 import Bottombar from "@/components/shared/Bottombar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import TopBar from "@/components/shared/Topbar";
-import { Outlet } from "react-router-dom";
+import { useUserContext } from "@/context/AuthContext";
+import { Navigate, Outlet } from "react-router-dom";
 
 const RootLayout = () => {
+  const { isAuthenticated } = useUserContext();
   return (
-    <div className="w-full md:flex">
-      <TopBar />
-      <LeftSidebar />
-      <section className="flex flex-1 h-full">
-        <Outlet />
-      </section>
-      <Bottombar />
-    </div>
+    <>
+      {isAuthenticated ? (
+        <div className="w-full md:flex">
+          <TopBar />
+          <LeftSidebar />
+          <section className="flex flex-1 h-full">
+            <Outlet />
+          </section>
+          <Bottombar />
+        </div>
+      ) : (
+        <Navigate to="/sign-in" />
+      )}
+    </>
   );
 };
 
